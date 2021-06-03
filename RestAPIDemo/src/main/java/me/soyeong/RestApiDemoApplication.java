90f39de2e0649ca8a85e611166c94c4f167c6513
User <User@DESKTOP-OO0B1CB>
2021-06-03 09:50:04 +0900

    commit sample

--- RestAPIDemo/src/main/java/me/soyeong/RestApiDemoApplication.java
+++ RestAPIDemo/src/main/java/me/soyeong/RestApiDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ui.ModelMap;
 
+
 @SpringBootApplication
 public class RestApiDemoApplication {
 
